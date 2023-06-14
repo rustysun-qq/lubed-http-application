9d1a3731a5b2382000a1d1a8421dc862b1948cbd
cjj <cjj8110_202302@126.com>
2023-06-14 12:35:52 +0800

    init http application FOR 0.2.0

--- src/DefaultDispatcher.php
+++ src/DefaultDispatcher.php
@@ -2,35 +2,32 @@
 namespace Lubed\HttpApplication;
 
 use Lubed\Http\Request;
-use Lubed\Container\Container;
+use Lubed\Utils\Registry;
 
 //TODO:???
-class DefaultDispatcher
-{
+class DefaultDispatcher {
     private $router;
 
-    public function __construct(Container &$app)
-    {
-        $this->router = $app->get('lubed_router_router');
-    }
-
-    public function dispatch(Request &$request) {
-        if(!$this->router){
-            AppExceptions::dispatchFailed('NOT FOUND ROUTER',[
+    public function __construct() {
+        $registry=Registry::getInstance();
+        $this->router=$registry->get('lubed_router_router');
+        if (!$this->router) {
+            AppExceptions::dispatchFailed('NOT FOUND ROUTER', [
                 'method'=>__METHOD__
             ]);
         }
+    }
 
-        $method = $request->getMethod();
-        $uri = $request->getUri();
-        $path = $uri->getPath();
-        $key = sprintf('%s %s',$method,$path);
-        $callee = $this->router->routing($key);
-
+    public function dispatch(Request &$request) {
+        $method=$request->getMethod();
+        $uri=$request->getUri();
+        $path=$uri->getPath();
+        $key=sprintf('%s %s', $method, $path);
+        $callee=$this->router->routing($key);
         if (!$callee) {
-           AppExceptions::dispatchFailed(sprintf('Routing Failed:INVALID PATH %s',$path),[
+            AppExceptions::dispatchFailed(sprintf('Routing Failed:INVALID PATH %s', $path), [
                 'method'=>__METHOD__
-            ]); 
+            ]);
         }
         $request->setRouted(true);
         return $callee;
